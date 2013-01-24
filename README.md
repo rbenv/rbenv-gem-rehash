a826dca3f13e77beabccd78157f6f3cf0a77d6eb
Sam Stephenson <sam@37signals.com>
2013-01-24 17:43:32 -0600

    Clarify the performance claim

--- README.md
+++ README.md
@@ -34,9 +34,10 @@ installed versions.
 If the `GEM_PATH` environment variable is undefined, rbenv-gem-rehash
 must first execute the `gem env gempath` command to retrieve RubyGems'
 default path so that it can can *append* to the path rather than
-override it. This can take several seconds, so the default path for
-the current Ruby version is cached to the filesystem the first time it
-is retrieved.
+override it. This can take a while—from a few hundred milliseconds on
+MRI to several seconds on JRuby—so the default path for the current
+Ruby version is cached to the filesystem the first time it is
+retrieved.
 
 ## License
 
--- etc/rbenv.d/exec/~gem-rehash.bash
+++ etc/rbenv.d/exec/~gem-rehash.bash
@@ -15,8 +15,9 @@
 # If the `GEM_PATH` environment variable is undefined,
 # rbenv-gem-rehash must first execute the `gem env gempath` command
 # to retrieve RubyGems' default path so that it can can *append* to
-# the path rather than override it. This can take several seconds, so
-# the default path for the current Ruby version is cached to the
+# the path rather than override it. This can take a while—from a few
+# hundred milliseconds on MRI to several seconds on JRuby—so the
+# default path for the current Ruby version is cached to the
 # filesystem the first time it is retrieved.
 
 # We set the `RBENV_GEM_REHASH` environment variable while running
